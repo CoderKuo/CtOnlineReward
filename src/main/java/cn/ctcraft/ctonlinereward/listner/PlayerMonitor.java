69c0e016a503c221cbf54e59eb74e62b9a65f6bb
Jin_ou <691422759@qq.com>
2021-08-11 20:08:40 +0800

    使用系统时间计算玩家在线时间 而非基于tick数进行计算 在服务器处于延迟状态时不影响实际计时

--- src/main/java/cn/ctcraft/ctonlinereward/listner/PlayerMonitor.java
+++ src/main/java/cn/ctcraft/ctonlinereward/listner/PlayerMonitor.java
@@ -1,13 +1,19 @@
 package cn.ctcraft.ctonlinereward.listner;
 
-import cn.ctcraft.ctonlinereward.CtOnlineReward;
+import cn.ctcraft.ctonlinereward.service.OnlineTimer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerMonitor implements Listener {
     @EventHandler
-    public void joinMonitor(PlayerJoinEvent event){
+    public void joinMonitor(PlayerJoinEvent event) {
+        OnlineTimer.addOnlinePlayer(event.getPlayer(), System.currentTimeMillis());
+    }
 
+    @EventHandler
+    public void quitMonitor(PlayerQuitEvent event) {
+        OnlineTimer.removeOnlinePlayer(event.getPlayer());
     }
 }
