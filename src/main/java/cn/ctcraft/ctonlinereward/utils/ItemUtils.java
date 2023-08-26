53606560f5785567877c732ee0bf76743193eb18
hotspringGG <78401087+hotspringGG@users.noreply.github.com>
2023-08-27 02:09:45 +0800

    1.20 if PASS!
    添加1.20 if

--- src/main/java/cn/ctcraft/ctonlinereward/utils/ItemUtils.java
+++ src/main/java/cn/ctcraft/ctonlinereward/utils/ItemUtils.java
@@ -39,7 +39,7 @@ public class ItemUtils {
         if (Bukkit.getVersion().contains("1.13") || Bukkit.getVersion().contains("1.14") ||
                 Bukkit.getVersion().contains("1.15") || Bukkit.getVersion().contains("1.16") ||
                 Bukkit.getVersion().contains("1.17") || Bukkit.getVersion().contains("1.18") ||
-                Bukkit.getVersion().contains("1.19"))
+                Bukkit.getVersion().contains("1.19") || Bukkit.getVersion().contains("1.20"))
             return false;
         return true;
     }
